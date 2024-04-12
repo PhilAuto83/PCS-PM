640073a61043840bad133ffa40e01daf97bdd1ad
Philip Mercan <philmercan@gmail.com>
2024-04-12 16:00:18 +0200

    adding info on custom authentication failure class

--- Poseidon-PM/src/main/java/com/phildev/pcs/config/CustomAuthenticationFailure.java
+++ Poseidon-PM/src/main/java/com/phildev/pcs/config/CustomAuthenticationFailure.java
@@ -9,6 +9,10 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 
 import java.io.IOException;
 
+/***
+ * This class is used to redirect users to login page with different error parameters
+ * It helps handling custom errors depending on whether this is a credentials error or session error
+ */
 public class CustomAuthenticationFailure implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
