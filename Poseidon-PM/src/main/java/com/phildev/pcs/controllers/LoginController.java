1a0b04ff937e7c2786248c301370a7578d915b96
Philip Mercan <philmercan@gmail.com>
2024-04-22 21:22:40 +0200

    adding javadoc to user service layer

--- Poseidon-PM/src/main/java/com/phildev/pcs/controllers/LoginController.java
+++ Poseidon-PM/src/main/java/com/phildev/pcs/controllers/LoginController.java
@@ -1,13 +1,10 @@
 package com.phildev.pcs.controllers;
 
-import com.phildev.pcs.repositories.UserRepository;
 import com.phildev.pcs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.security.Principal;
 
 @Controller
 public class LoginController {
