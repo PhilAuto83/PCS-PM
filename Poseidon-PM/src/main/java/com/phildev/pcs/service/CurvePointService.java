a3c5c0276c39740ec415e4955a209f3ab19aac4c
Philip Mercan <philmercan@gmail.com>
2024-04-22 17:12:34 +0200

    adding javadoc to service layer rule, bid , trade, curve point, rating

--- Poseidon-PM/src/main/java/com/phildev/pcs/service/CurvePointService.java
+++ Poseidon-PM/src/main/java/com/phildev/pcs/service/CurvePointService.java
@@ -1,7 +1,9 @@
 package com.phildev.pcs.service;
 
 
+import com.phildev.pcs.domain.BidList;
 import com.phildev.pcs.domain.CurvePoint;
+import com.phildev.pcs.repositories.BidListRepository;
 import com.phildev.pcs.repositories.CurvePointRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +13,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+
+/**
+ * This class is the service used to communicate between {@link com.phildev.pcs.controllers.CurveController} and {@link com.phildev.pcs.repositories.BidListRepository}
+ * All the methods in this service helps saving, deleting, retrieving curve point in database
+ */
 @Service
 public class CurvePointService {
 
@@ -19,6 +26,11 @@ public class CurvePointService {
     @Autowired
     private CurvePointRepository curvePointRepository;
 
+
+    /**
+     * This method calls bid repository with its {@link CurvePointRepository#findAll()} method to find all curve points in database
+     * @return a list of CurvePoint
+     */
     public List<CurvePoint> findAll(){
         List<CurvePoint> allCurvePoints = curvePointRepository.findAll();
         if(allCurvePoints.isEmpty()){
@@ -27,15 +39,31 @@ public class CurvePointService {
         return allCurvePoints;
     }
 
+
+    /**
+     * This method saves a {@link CurvePoint} in database
+     * @param curvePoint with its properties term , value that will be saved in database
+     * @return a curve point which was saved successfully in database
+     */
     public CurvePoint save(CurvePoint curvePoint){
         return curvePointRepository.save(curvePoint);
     }
 
+    /**
+     * This method takes the id of a curve point we want to find and the method {@link CurvePointRepository#findById(Object)} will retrieve the curve point or not owing to Optional type
+     * @param id which is the unique curve point identifier
+     * @return an optional of curvePoint so it can be empty or return something
+     */
     public Optional<CurvePoint> findById(Integer id){
         return curvePointRepository.findById(id);
     }
 
 
+    /**
+     * This method takes the id of a curve point we want to delete and the method {@link CurvePointRepository#deleteById(Object)} will delete the curve point
+     * @param id which is the unique curve point identifier
+     * @return void
+     */
     public void delete(Integer id){
         curvePointRepository.deleteById(id);
     }
--- Poseidon-PM/src/main/java/com/phildev/pcs/service/RatingService.java
+++ Poseidon-PM/src/main/java/com/phildev/pcs/service/RatingService.java
@@ -1,6 +1,8 @@
 package com.phildev.pcs.service;
 
+import com.phildev.pcs.domain.CurvePoint;
 import com.phildev.pcs.domain.Rating;
+import com.phildev.pcs.repositories.CurvePointRepository;
 import com.phildev.pcs.repositories.RatingRepository;
 import com.phildev.pcs.repositories.TradeRepository;
 import org.slf4j.Logger;
@@ -11,6 +13,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+
+/**
+ * This class is the service used to communicate between {@link com.phildev.pcs.controllers.RatingController} and {@link RatingRepository}
+ * All the methods in this service helps saving, deleting, retrieving rating in database
+ */
 @Service
 public class RatingService {
 
@@ -22,6 +29,10 @@ public class RatingService {
     @Autowired
     private TradeRepository tradeRepository;
 
+    /**
+     * This method calls {@link RatingRepository#findAll()} method to find all ratings in database
+     * @return a list of Rating
+     */
     public List<Rating> findAll(){
         List<Rating> allRatings = ratingRepository.findAll();
         if(allRatings.isEmpty()){
@@ -30,28 +41,58 @@ public class RatingService {
         return allRatings;
     }
 
+    /**
+     * This methods checks if a {@link com.phildev.pcs.domain.Trade#tradeId} exists with the {@link Rating#orderNumber} in database
+     * @param rating
+     * @return true or false if trade id exists
+     */
     public boolean checkOrderNumberExistInTrade(Rating rating){
         // checking trade id matches orderNumber to add a rating
         return tradeRepository.existsById(rating.getOrderNumber());
     }
 
+    /**
+     * This methods checks if a rating already exists in database with the same agency ratings (moodys, fitch and S and P)
+     * @param rating
+     * @return true or false if rating exists
+     */
     public boolean checkRatingExists(Rating rating){
         Optional<Rating> ratingFound = ratingRepository.findByOrderNumber(rating.getOrderNumber());
         return ratingFound.isPresent();
     }
 
+    /**
+     * This method saves a {@link Rating} in database
+     * @param rating with its properties {@link Rating#moodysRating} , {@link Rating#fitchRating}, {@link Rating#sandPRating} and {@link Rating#orderNumber} that will be saved in database
+     * @return a rating which was saved successfully in database
+     */
     public Rating save(Rating rating){
         return ratingRepository.save(rating);
     }
 
+    /**
+     * This method takes the id of a rating we want to find and the method {@link RatingRepository#findById(Object)} will retrieve the rating or not owing to Optional type
+     * @param id which is the unique rating identifier
+     * @return an optional of rating so it can be empty or return something
+     */
     public Optional<Rating> findById(Integer id){
         return ratingRepository.findById(id);
     }
 
+    /**
+     * This method takes the id of a rating we want to find and the method {@link RatingRepository#findById(Object)} will find the rating
+     * @param id which is the unique rating identifier
+     * @return an optional of Rating which can be empty or have a rating
+     */
     public Optional<Rating> findByOrderNumber(Integer id){
         return ratingRepository.findByOrderNumber(id);
     }
 
+    /**
+     * This method takes the id matching a trade id we want to delete and the method {@link RatingRepository#deleteById(Object)} will delete the rating
+     * @param id which is the unique rating identifier
+     * @return void
+     */
     public void delete(Integer id){
         ratingRepository.deleteById(id);
     }
